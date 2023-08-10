6b81dcc32b9fb0ff39816ebe6932deb529d22242
serverless-devs <serverless-devs@alibaba-inc.com>
2023-08-10 03:04:02 +0000

    "代理地址修改"

--- code/app.js
+++ code/app.js
@@ -1,21 +1,51 @@
-  const express = require('express')
-  const {
-    createProxyMiddleware
-  } = require('http-proxy-middleware');
-  const app = express()
-  const port = 9000
-  const openAiSecret = process.env.openAiSecret;
-  console.log(openAiSecret);
+const express = require('express')
+const {
+  createProxyMiddleware
+} = require('http-proxy-middleware');
+const app = express()
+const port = 9000
 
 
-  app.use('/', createProxyMiddleware({
-    target: 'https://api.openai.com',
+app.use('/api',
+  createProxyMiddleware({
+    target: 'https://discord.com/',
     changeOrigin: true,
-    onProxyRes: function (proxyRes, req, res) {
-      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    }
-  }));
-
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
   })
+);
+
+// app.use('/ws',
+//   createProxyMiddleware({
+//     target: 'ws://gateway.discord.gg',
+//     ws:true,
+//     changeOrigin: true,
+//   })
+// );
+
+app.use('/mj', createProxyMiddleware({
+  target: 'https://midjourney-proxy-production-qsx.up.railway.app',
+  changeOrigin: true,
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+app.use('/mjdy', createProxyMiddleware({
+  target: 'https://midjourney-proxy-production-5c01.up.railway.app',
+  changeOrigin: true,
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+
+app.use('/', createProxyMiddleware({
+  target: 'https://api.openai.com',
+  changeOrigin: true,
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
